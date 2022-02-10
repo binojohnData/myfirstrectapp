71a130d86a9d0bd94187fd1c8b538443088347e0
binojohn79 <>
2022-02-11 08:19:03 +1100

    changes to react app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello RINO. OMGGGGGGGG!!!
+          Hello RINO. OMGGGGGGGG!!! Life is Amazing!!!
         </p>
         <a
           className="App-link"
